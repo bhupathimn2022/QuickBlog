ac18392e77b8026cf7b80c993f387fc7a17376da
bhupathimn2022 <125240993+bhupathimn2022@users.noreply.github.com>
2025-08-12 19:15:10 +0530

    Blog page Design completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import { Blog } from './pages/Blog'
+import  Blog  from './pages/Blog'
 const App = () => {
   return (
     <div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
       <Newsletter />
       <Footer />
       
+      
       </>
   )
 }
